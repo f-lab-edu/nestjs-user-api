8e1e0e746e4463ab6620f85f20420b0126230e60
Yujin Min <yujinmin91@gmail.com>
2024-01-29 10:45:21 +0900

    rename entity folder to entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entity/user.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
